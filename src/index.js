758f33969c71e9992b333d01e655da288cfb87df
Luis Faria <luiscarlossfaria@gmail.com>
2022-05-03 15:51:16 -0300

    Made the logo larger

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   
-        <BrowserRouter basename={process.env.PUBLIC_URL} >
+        <BrowserRouter >
           <App />
         </BrowserRouter>,
       
