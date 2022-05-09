56c884a894d6d3b0cd0d8ca1ef452def801a7f5b
Luis Faria <luiscarlossfaria@gmail.com>
2022-05-09 14:11:07 -0300

    Added contact page

--- src/pages/contact/contact-page.component.jsx
+++ src/pages/contact/contact-page.component.jsx
@@ -13,6 +13,37 @@ class ContactPage extends Component {
         return (
             <div>
                 <Header/>
+                <div className="contact-page">
+                        <h3 className="contact-title">Fale com a gente:</h3>
+                    <div className="contact-box">
+                        <div className="form">
+                            <div className="dados">
+                                <div className="field">
+                                    <i className="fa-solid fa-user"></i>
+                                    <input className="input" type="text" placeholder="Nome"></input>
+                                </div>
+                                <div className="field">
+                                    <i className="fa-solid fa-phone"></i>
+                                    <input className="input" type="text" placeholder="Telefone"></input>
+                                </div>
+                                <div className="field">
+                                    <i className="fa-solid fa-envelope"></i>
+                                    <input className="input" type="email" placeholder="e-mail"></input>
+                                </div>
+                            </div>
+                            <div className="field message">
+                                    <i className="fa-solid fa-message"></i>
+                                    <textarea className="text-area" type="text" placeholder="Mensagem"></textarea>
+                            </div>
+                            <button className="button">ENVIAR</button>                                                 
+                            
+
+                        </div>
+                    </div>
+
+
+
+                </div>
                 
                 <Footer/>
                 
